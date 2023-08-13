d41ec6ba92dd5313ad63f1c2b88745bcb029a9b5
Jalal Hosseini <jalalhosseiny@gmail.com>
2023-08-13 22:18:17 +0100

    add tokenizer and parser

--- backend/lib/fzf/src/pattern.zig
+++ backend/lib/fzf/src/pattern.zig
@@ -3,8 +3,81 @@ const std = @import("std");
 // const options = @import("fzf_options");
 const options = .{ .max_pattern_len = 64, .max_sub_pattern_size = 8 };
 
+const Token = union(enum) {
+    case_sensitive: void,
+    space: void,
+    single_quote: void,
+    incomplete: void,
+
+    char: u8,
+};
+
+pub fn parse(pattern: []const u8) Pattern {
+    var ptrn = Pattern{};
+
+    // Tokenizer
+    var tokens: [options.max_pattern_len]Token = undefined;
+    var ti: usize = 0;
+    var i: usize = 0;
+    while (i < pattern.len) : (i += 1) {
+        const p = pattern[i];
+        tokens[ti] = switch (p) {
+            ' ' => Token.space,
+            '\\' => blk: {
+                if (i + 1 == pattern.len) break :blk Token.incomplete;
+                if (pattern[i + 1] == ' ') {
+                    i += 1; // consume one
+                    break :blk Token{ .char = ' ' };
+                } else if (pattern[i + 1] == '\'') {
+                    i += 1; // consume one
+                    break :blk Token{ .char = '\'' };
+                }
+                break :blk Token{ .char = p };
+            },
+            '\'' => if (i == 0 or tokens[ti - 1] == Token.space) Token.single_quote else Token{ .char = '\'' },
+            else => Token{ .char = p },
+        };
+
+        ti += 1;
+    }
+    const token_len = ti;
+
+    // Parser
+    var chunk_no: usize = 0;
+    ti = 0;
+    var buf_i: usize = 0;
+    var buf_offset: usize = 0;
+
+    while (ti < token_len) : ({
+        ti += 1;
+        chunk_no += 1;
+    }) {
+        var chunk = Pattern.Chunk{};
+        var chunk_size: usize = 0;
+        while (ti < token_len) : (ti += 1) {
+            switch (tokens[ti]) {
+                Token.space => break,
+                Token.single_quote => {
+                    chunk.is_fuzzy = false;
+                },
+                Token.char => |ch| {
+                    ptrn.buf[buf_i] = ch;
+                    buf_i += 1;
+                    chunk_size += 1;
+                },
+                else => {},
+            }
+        }
+        chunk.pattern = ptrn.buf[buf_offset .. buf_offset + chunk_size];
+        buf_offset += chunk_size;
+        ptrn.chunks[chunk_no] = chunk;
+    }
+
+    return ptrn;
+}
+
 pub const Pattern = struct {
-    pattern_buf: [options.max_pattern_len]u8 = undefined,
+    buf: [options.max_pattern_len]u8 = undefined,
     sub_patterns: [options.max_sub_pattern_size][options.max_pattern_len]u8 = undefined,
     chunks: [options.max_sub_pattern_size]Chunk = undefined,
 
@@ -13,56 +86,72 @@ pub const Pattern = struct {
         is_case_insensitive: bool = true,
         is_fuzzy: bool = true,
     };
-
-    const Self = @This();
-    pub fn init() Self {
-        return .{};
-    }
-
-    fn parse(self: *Self, pattern: []const u8) void {
-        var pi: usize = 0;
-        var sub_pi: usize = 0;
-
-        while (pi < pattern.len) : (pi += 1) {
-            // self.pattern_buf[pi] = pattern[pi];
-
-            var chunk_ch_i: usize = 0;
-            var chunk_offset = pi;
-            var chunk = Chunk{};
-
-            while (pi < pattern.len and pattern[pi] != ' ') : ({
-                pi += 1;
-                chunk_ch_i += 1;
-            }) {
-                self.pattern_buf[chunk_offset + chunk_ch_i] = pattern[pi];
-            }
-            chunk.pattern = self.pattern_buf[chunk_offset..chunk_offset + chunk_ch_i];
-
-            self.chunks[sub_pi] = chunk;
-            sub_pi += 1;
-        }
-    }
 };
 
 const testing = std.testing;
 const eq = testing.expectEqual;
 const sliceEq = testing.expectEqualSlices;
 test "pattern" {
-    var p = Pattern.init();
     {
-        const pattern = "foo";
-        p.parse(pattern);
+        const p = parse("foo");
 
         const ck = p.chunks[0];
-        try sliceEq(u8, pattern, ck.pattern);
+        try sliceEq(u8, "foo", ck.pattern);
         try eq(true, ck.is_fuzzy);
         try eq(true, ck.is_case_insensitive);
     }
-    {
-        const pattern = "foo bar";
-        p.parse(pattern);
+    { // space -> chunk delimiter
+        var p = parse("foo bar");
 
         try sliceEq(u8, "foo", p.chunks[0].pattern);
+        try sliceEq(u8, "bar", p.chunks[1].pattern);
+
+        p = parse(" foo");
+        try sliceEq(u8, "", p.chunks[0].pattern);
+        try sliceEq(u8, "foo", p.chunks[1].pattern);
+
+        p = parse(" 'foo");
+        try sliceEq(u8, "", p.chunks[0].pattern);
+        try eq(true, p.chunks[0].is_fuzzy);
+
+        try sliceEq(u8, "foo", p.chunks[1].pattern);
+        try eq(false, p.chunks[1].is_fuzzy);
+
+        p = parse("' foo");
+        try sliceEq(u8, "", p.chunks[0].pattern);
+        try eq(false, p.chunks[0].is_fuzzy);
+        try sliceEq(u8, "foo", p.chunks[1].pattern);
+    }
+    { // escape
+        var p = parse("\\ foo\\ bar");
+        try sliceEq(u8, " foo bar", p.chunks[0].pattern);
+
+        p = parse("\\'foo\\ bar");
+        try sliceEq(u8, "'foo bar", p.chunks[0].pattern);
+
+        p = parse("a\\b");
+        try sliceEq(u8, "a\\b", p.chunks[0].pattern);
+    }
+    { // exact match
+        var p = parse("'foo");
+
+        try eq(false, p.chunks[0].is_fuzzy);
+        try sliceEq(u8, "foo", p.chunks[0].pattern);
+
+        p = parse("fo'o");
+        try eq(true, p.chunks[0].is_fuzzy);
+        try sliceEq(u8, "fo'o", p.chunks[0].pattern);
+
+        p = parse("'foo'");
+        try eq(false, p.chunks[0].is_fuzzy);
+        try sliceEq(u8, "foo'", p.chunks[0].pattern);
+    }
+    { // multi-chunk
+        var p = parse("'foo 'bar");
+
+        try eq(false, p.chunks[0].is_fuzzy);
+        try sliceEq(u8, "foo", p.chunks[0].pattern);
+        try eq(false, p.chunks[1].is_fuzzy);
         try sliceEq(u8, "bar", p.chunks[1].pattern);
     }
 }
