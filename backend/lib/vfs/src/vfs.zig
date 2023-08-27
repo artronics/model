14015248346a7df49b6b50f56793e7f6cfd39fd0
artronics <jalalhosseiny@gmail.com>
2023-08-27 22:00:50 +0100

    add path buffer

--- backend/lib/vfs/src/vfs.zig
+++ backend/lib/vfs/src/vfs.zig
@@ -1,7 +1,129 @@
 const std = @import("std");
+const fs = std.fs;
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
+const ArenaAllocator = std.heap.ArenaAllocator;
 
 const testing = std.testing;
 const eq = std.testing.expectEqual;
+
+const Vfs = struct {
+    arena: ArenaAllocator,
+    path_buf: PathBuffer,
+    const Self = @This();
+
+    pub fn init(allocator: Allocator, path: []const u8) !Self {
+        var arena = ArenaAllocator.init(allocator);
+        const arenaAlloc = arena.allocator();
+
+        var d = try fs.openDirAbsolute(path, .{ .access_sub_paths = true, .no_follow = true });
+        defer d.close();
+        const id = fs.IterableDir{ .dir = d };
+        var walker = try id.walk(arenaAlloc);
+        defer walker.deinit();
+
+        return Self{
+            .arena = arena,
+            .path_buf = try PathBuffer.init(arenaAlloc),
+        };
+    }
+    pub fn deinit(self: Self) void {
+        self.arena.deinit();
+    }
+
+    /// buffer used for storing paths
+    /// it doesn't store a reference to the allocated memory i.e. it leaks memory. Only use it with ArenaAllocator.
+    // TODO: Probably using an ArrayList with adjusting all pointers' offset in case of realloc is a better choice?
+    const PathBuffer = struct {
+        allocator: Allocator,
+        cur_buf: []u8 = undefined,
+        cur_index: usize = 0,
+
+        const cap: usize = 1024;
+
+        fn init(allocator: Allocator) !PathBuffer {
+            var self = PathBuffer{ .allocator = allocator };
+            self.cur_buf = try allocator.alloc(u8, cap);
+
+            return self;
+        }
+
+        fn appendSlice(ss: *PathBuffer, s: []const u8) ![]const u8 {
+            const left = cap - ss.cur_index;
+            if (s.len > left) {
+                ss.cur_buf = try ss.allocator.alloc(u8, cap);
+                ss.cur_index = 0;
+            }
+
+            const offset = ss.cur_index;
+            var i: usize = 0;
+            while (i < s.len) : ({
+                i += 1;
+                ss.cur_index += 1;
+            }) {
+                ss.cur_buf[ss.cur_index] = s[i];
+            }
+
+            return ss.cur_buf[offset .. offset + s.len];
+        }
+    };
+
+    test "PathBuffer" {
+        var arena = ArenaAllocator.init(testing.allocator);
+        defer arena.deinit();
+
+        var ss = try PathBuffer.init(arena.allocator());
+        const exp_x = "x" ** PathBuffer.cap;
+        const exp_y = "y" ** PathBuffer.cap;
+
+        const sx = try ss.appendSlice(exp_x);
+        const sy = try ss.appendSlice(exp_y);
+        const foo = try ss.appendSlice("foo");
+        const bar = try ss.appendSlice("bar");
+
+        try testing.expectEqualSlices(u8, exp_x, sx);
+        try testing.expectEqualSlices(u8, exp_y, sy);
+        try testing.expectEqualSlices(u8, "foo", foo);
+        try testing.expectEqualSlices(u8, "bar", bar);
+    }
+};
+
 test "vfs" {
-    try eq(true, true);
+    const a = testing.allocator;
+
+    var tmp_dir = testing.tmpDir(.{});
+    defer tmp_dir.cleanup();
+    try makeTestData(tmp_dir);
+
+    var buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+    var d = try tmp_dir.dir.realpath("root", &buf);
+
+    const vfs = try Vfs.init(a, d);
+    defer vfs.deinit();
+    std.log.warn("root path: {s}", .{d});
+}
+
+/// create a simple nested directory structure for testing
+/// /root
+///   /empty
+///   /a
+///     /b
+///       c.txt
+///   b.txt
+///   /c
+///     c1.txt
+///     c2.txt
+///   /same
+///     /same
+///       /same
+///
+fn makeTestData(dir: testing.TmpDir) !void {
+    try dir.dir.makePath("root/empty");
+    try dir.dir.makePath("root/a/b");
+    try dir.dir.makePath("root/c");
+    try dir.dir.makePath("root/same/same/same");
+    _ = try dir.dir.createFile("root/b.txt", .{});
+    _ = try dir.dir.createFile("root/c/c1.txt", .{});
+    _ = try dir.dir.createFile("root/c/c2.txt", .{});
+    _ = try dir.dir.createFile("root/a/b/c.txt", .{});
 }
