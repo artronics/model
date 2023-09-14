81a5c8940cb1d0762d92ef8dec3094498a5b4e37
artronics <jalalhosseiny@gmail.com>
2023-09-14 23:16:53 +0100

    create ordered results

--- backend/lib/fzf/src/main.zig
+++ backend/lib/fzf/src/main.zig
@@ -7,6 +7,7 @@ const chk = @import("chunk.zig");
 const Chunk = chk.Chunk;
 const testing = std.testing;
 const expect = testing.expect;
+const sliceEq = testing.expectEqualSlices;
 
 pub const match = algo.match;
 pub const MatchType = chk.MatchType;
@@ -17,27 +18,61 @@ pub const MatchFinder = struct {
     allocator: Allocator,
     term: term.Term,
     smart_case: bool = false,
-    haystack: []const []const u8,
+    texts: []const []const u8 = undefined,
 
     pub fn init(allocator: Allocator, texts: []const []const u8) Self {
         return .{
             .allocator = allocator,
             .term = term.Term.init(allocator),
-            .haystack = texts,
+            .texts = texts,
         };
     }
     pub fn deinit(self: Self) void {
         self.term.deinit();
     }
-    pub fn search(self: *Self, term_str: []const u8) !?isize {
+
+    const Result = struct {
+        text: []const u8,
+        score: isize,
+
+        fn sortByScore(context: void, a: Result, b: Result) bool {
+            _ = context;
+            return a.score > b.score;
+        }
+    };
+
+    /// client owns the result slice.
+    pub fn search(self: *Self, term_str: []const u8) ![]const []const u8 {
         try self.term.parse(term_str);
-        return self.reduceScore(null, self.term.expr);
+
+        var results = ArrayList(Result).init(self.allocator);
+        defer results.deinit();
+        for (self.texts) |text| {
+            if (self.reduceScore(text, null, self.term.expr)) |score| {
+                const res = Result{ .text = text, .score = score };
+                try results.append(res);
+            }
+        }
+
+        // sort based on score and copy only texts for final results
+        std.sort.heap(Result, results.items, {}, Result.sortByScore);
+        var sorted = try self.allocator.alloc([]const u8, results.items.len);
+        std.log.warn("------------", .{});
+        for (results.items, 0..results.items.len) |item, i| {
+            sorted[i] = item.text;
+            std.log.warn("[{d}]: {s}", .{ item.score, item.text });
+        }
+
+        return sorted;
     }
-    fn reduceScore(self: Self, acc: ?isize, expr: term.Expr) ?isize {
+    fn reduceScore(self: Self, text: []const u8, acc: ?isize, expr: term.Expr) ?isize {
         // short circuit both "and" and "or" by evaluating left branch first
         return switch (expr) {
             term.Expr.chunk => |chunk| {
-                return if (self.matchChunk(chunk)) |chunk_s| {
+                const case_sen = self.smart_case and chk.caseVaries(chunk.pattern);
+                const score = match(text, chunk.pattern, case_sen, chunk.match_type);
+
+                return if (score) |chunk_s| {
                     return if (acc) |acc_s| {
                         // FIXME: Should it be addition or max? same for other branches
                         // return ss +| s;
@@ -46,9 +81,9 @@ pub const MatchFinder = struct {
                 } else null;
             },
             term.Expr.and_op => |op| {
-                const _ls = self.reduceScore(acc, op.l.*);
+                const _ls = self.reduceScore(text, acc, op.l.*);
                 return if (_ls) |ls| {
-                    const _rs = self.reduceScore(ls, op.r.*);
+                    const _rs = self.reduceScore(text, ls, op.r.*);
                     return if (_rs) |rs| {
                         // return s +| ss;
                         return @max(ls, rs);
@@ -56,30 +91,80 @@ pub const MatchFinder = struct {
                 } else null;
             },
             term.Expr.or_op => |op| {
-                const _ls = self.reduceScore(acc, op.l.*);
+                const _ls = self.reduceScore(text, acc, op.l.*);
                 return if (_ls) |ls| {
                     return ls;
                 } else {
-                    const _rs = self.reduceScore(acc, op.r.*);
+                    const _rs = self.reduceScore(text, acc, op.r.*);
                     return if (_rs) |rs| rs else null;
                 };
             },
         };
     }
-    fn matchChunk(self: Self, chunk: Chunk) ?isize {
-        const case_sen = self.smart_case and chk.caseVaries(chunk.pattern);
-
-        return match(self.haystack[0], chunk.pattern, case_sen, chunk.match_type);
-    }
 };
 
 test "Matcher" {
     const a = testing.allocator;
-    const texts = [_][]const u8{"foo bar baz"};
-    // const texts = [_][]const u8{ "bite continue delightful", "earthwax summary emotion", "sticks notify banish" };
-    var matcher = MatchFinder.init(a, &texts);
+    // ? is used to make a distinction when we make assertions. They don't matter
+    // ^ to reduce the score for order test
+    // $ to eliminate boundary
+    const texts = &[_][]const u8{ "unique", "foo", "foobar", "barfoo", "$order^^^", "xyz_", "abxy", "$order^", "$order^^" };
+    var matcher = MatchFinder.init(a, texts);
     defer matcher.deinit();
 
-    const score = try matcher.search("foo | bar baz");
-    try expect(score != null);
+    {
+        const results = try matcher.search("unique");
+        defer a.free(results);
+        try expect(results.len == 1);
+        try sliceEq(u8, "unique", results[0]);
+    }
+    { // order
+        const results = try matcher.search("order");
+        defer a.free(results);
+        try expect(results.len == 3);
+        try sliceEq(u8, "$order^", results[0]);
+        try sliceEq(u8, "$order^^", results[1]);
+        try sliceEq(u8, "$order^^^", results[2]);
+    }
+    { // exact, suffix and prefix
+        const exact = try matcher.search("'foo");
+        defer a.free(exact);
+        try expect(exact.len == 3);
+
+        const prefix = try matcher.search("^foo");
+        defer a.free(prefix);
+        try expect(prefix.len == 2);
+        try sliceEq(u8, "foobar", prefix[1]);
+
+        const suffix = try matcher.search("foo$");
+        defer a.free(suffix);
+        try expect(suffix.len == 2);
+        try sliceEq(u8, "barfoo", suffix[1]);
+    }
+    { // inverse: exact, suffix and prefix
+        const inverse = try matcher.search("!unique");
+        defer a.free(inverse);
+        try expect(inverse.len == texts.len - 1);
+
+        const in_pre = try matcher.search("!^foo");
+        defer a.free(in_pre);
+        try expect(in_pre.len == texts.len - 2);
+
+        const in_suf = try matcher.search("!foo$");
+        defer a.free(in_suf);
+        try expect(in_suf.len == texts.len - 2);
+    }
+    {
+        const or_bin = try matcher.search("unique | foo");
+        defer a.free(or_bin);
+        try expect(or_bin.len == 4);
+
+        const and_bin = try matcher.search("foo bar");
+        defer a.free(and_bin);
+        try expect(and_bin.len == 2);
+
+        const or_and = try matcher.search("foo foo | bar");
+        defer a.free(or_and);
+        try expect(or_and.len == 3);
+    }
 }
